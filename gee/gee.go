14ca34f80b240b3e08c9db86922f900d152577ed
Your Name <yangbaixin200211@163.com>
2023-03-18 09:59:45 +0800

    4th day, add GroupRouter

--- gee/gee.go
+++ gee/gee.go
@@ -1,17 +1,55 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
 type HandlerFunc func(c *Context)
 
 type Engine struct {
+	*RouteGroup
 	routers *router
+	groups  []*RouteGroup
+}
+
+type RouteGroup struct {
+	prefix      string
+	engine      *Engine
+	middlewares []HandlerFunc
+	parent      *RouteGroup
 }
 
 func New() *Engine {
-	return &Engine{routers: newRouter()}
+	engine := &Engine{routers: newRouter()}
+	engine.RouteGroup = &RouteGroup{engine: engine}
+	engine.groups = []*RouteGroup{engine.RouteGroup}
+	return engine
+}
+
+func (g *RouteGroup) Group(prefix string) *RouteGroup {
+	engine := g.engine
+	newGroup := &RouteGroup{
+		prefix: g.prefix + prefix,
+		engine: engine,
+		parent: g,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouteGroup) addRoute(method string, comp string, handler HandlerFunc) {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.routers.addRoute(method, pattern, handler)
+}
+
+func (group *RouteGroup) GET(path string, handler HandlerFunc) {
+	group.addRoute("GET", path, handler)
+}
+
+func (group *RouteGroup) POST(path string, handler HandlerFunc) {
+	group.addRoute("POST", path, handler)
 }
 
 func (e *Engine) addRouter(method, path string, handler HandlerFunc) {
